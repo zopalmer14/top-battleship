13b16e77ca395641e28002c0d1fa482f5b197f49
zopalmer14 <zopalmer14@gmail.com>
2024-04-11 12:40:51 -0400

    create Ship object factory

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
-import './style.css';
+
+// Ship Factory
+const createShip = function createShip(length) {
+    let times_hit = 0;
+
+    // getters 
+    const shipDamage = () => times_hit;
+    const shipLength = () => length;
+
+    // setters 
+    const hit = () => times_hit++;
+
+    const isSunk = () => times_hit >= length;
+
+    return {  
+        shipDamage, 
+        shipLength,
+        hit,
+        isSunk,
+    }
+}
