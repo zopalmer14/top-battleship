b55b28567c824d223a1c1c210ead497764ea6ed3
zopalmer14 <zopalmer14@gmail.com>
2024-04-18 12:54:52 -0400

    create function to automatically place ships in default locations on a board

--- src/index.js
+++ src/index.js
@@ -9,6 +9,10 @@ import {
 
 import { createPlayer } from './player.js';
 
+import { createShip } from './ship.js';
+
+const BOARD_SIZE = 10;
+
 const pageInterface = function pageInterface() {
     let P1;
     const CPU = createPlayer('CPU', true);
@@ -19,6 +23,7 @@ const pageInterface = function pageInterface() {
         setupStartGame();
     }
 
+    // JS logic for the start game UI
     function setupStartGame() {
         // dom references
         const startGameButton = document.querySelector('header > button');
@@ -34,14 +39,40 @@ const pageInterface = function pageInterface() {
         startGameForm.addEventListener('submit', (event) => {
             // create a new Player with the form info
             P1 = createPlayer(event.target.playerName.value, false);
+            console.log(P1);
 
             // create two boards in the DOM -- one for the user and one for the CPU
-            setupGameBoard(8);
-            setupGameBoard(8);
+            setupGameBoard(BOARD_SIZE);
+            setupGameBoard(BOARD_SIZE);
+
+            // setup the boards in the default positions
+            defaultShipPlacements(P1.getPlayerBoard());
+            defaultShipPlacements(CPU.getPlayerBoard());
 
             // reset the form inputs
             startGameForm.reset(); 
         });
+    }
+
+    // helper / debug function that automatically sets up the default ship positions for the given board
+    function defaultShipPlacements(board) {
+        // default ships
+        const battleship = createShip('Battleship', 5);
+        const cruiser = createShip('Cruiser', 4);
+        const sub = createShip('Sub', 3);
+        const patrolBoat = createShip('Patrol Boat', 2);
+
+        // battleship from (0, 0) to (0, 4)
+        board.placeShip(battleship, 0, 0, false);
+
+        // cruiser from (6, 9) to (9, 9)
+        board.placeShip(cruiser, 6, 9, true);
+
+        // sub from (0, 7) to (0, 9)
+        board.placeShip(sub, 0, 7, false);
+
+        // patrol boat from (8, 0) to (9, 0)
+        board.placeShip(patrolBoat, 8, 0, true);
     }
     
     return {
--- src/player.js
+++ src/player.js
@@ -4,9 +4,11 @@ import {
     createGameBoard,
 } from './gameBoard.js';
 
+const BOARD_SIZE = 10;
+
 // Player object factory function
 const createPlayer = function createPlayer(playerName, isComputer) {
-    const playerBoard = createGameBoard(8);
+    const playerBoard = createGameBoard(BOARD_SIZE);
 
     // getters
     const name = () => playerName;
